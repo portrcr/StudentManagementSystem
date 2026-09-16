75f703a7bcd4ba1041c949926d49f3ce358a7253
portrcr <portrcr@gmail.com>
2026-09-16 18:20:02 +0300

    PDO connection to database

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+	$host = "localhost";
+	$dbname = "student_db";
+	$username = "root";
+	$password = "";
+
+	try {
+		$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, [
+			PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC,
+			PDO::ATTR_EMULATE_PREPARES		=> false,
+		]);
+	}	catch(PDOException $e) {
+		die("Database connection failed: " . $e->getMessage());
+	}
+	
