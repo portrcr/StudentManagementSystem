f27d7f7be7278cd9509fa01278e2aabb8aeb6cd9
portrcr <portrcr@gmail.com>
2026-09-17 14:07:36 +0300

    Added link to stylesheet

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,17 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php	echo $title;	?></title>
+	<title><?php echo $title ?? "Student Management System"; ?></title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+	<header>
+		<nav>
+			<a href="index.php">Dashboard</a>
+			<a href="studentlist.php">List Of Enrolled Students</a>
+			<a href="create.php">Add New Student</a>
+			<a href="edit.php">Modify A Students Data</a>
+			<a href="delete.php">Remove A Student</a>
+		</nav>
+	</header>
